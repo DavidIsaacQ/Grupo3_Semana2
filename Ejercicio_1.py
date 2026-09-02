9b3b0f629335322835d6be090fb4ebe8bc7fb569
DavidIsaacQ <davidquispe60@gmail.com>
2026-09-01 23:22:16 -0500

    Ejercicio 01 — Área del Rectángulo

---
+++ Ejercicio_1.py
@@ -0,0 +1,7 @@
+base = float(input("Ingresar la base del triangulo: "))
+altura = float(input("Ingresar la altura de triangulo: "))
+
+area = base*altura
+perimetro= 2*base+2*altura
+
+print(f"El area es: {area:.2f} y el perimetro es: {perimetro:.2f}")
